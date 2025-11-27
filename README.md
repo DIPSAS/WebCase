a95b17e802742fce77ac2fbabdf1a53cfeab4222
Birk Eidsvik <bbe@dips.no>
2025-11-27 11:22:53 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+--- Hva tenker du om håndtering av pakkeversjoner i dette monorepoet?
+    --- Dette monorepoet bruker både react 18 og react 19. Har dette noe å si? 
+
+---
+
+Alle pakker i dette monorepoet må bygges. Hva tenker du er pros og cons med dette og skalerer dette bra etterhvert som mengden pakker øker?
+
+--- Hvordan tenker du om deling av konfigurasjon (f.eks. eslint, prettier, babel, webpack) på tvers av pakkene i monorepoet?
---
+++ apps/storefront/eslint.config.js
@@ -0,0 +1,77 @@
+import js from "@eslint/js";
+import { globalIgnores } from "eslint/config";
+import tseslint from "typescript-eslint";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+import pluginReact from "eslint-plugin-react";
+import globals from "globals";
+import pluginNext from "@next/eslint-plugin-next";
+import eslintConfigPrettier from "eslint-config-prettier";
+import turboPlugin from "eslint-plugin-turbo";
+import onlyWarn from "eslint-plugin-only-warn";
+
+const config = [
+  js.configs.recommended,
+  eslintConfigPrettier,
+  ...tseslint.configs.recommended,
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ["eslint.config.js"],
+        },
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    plugins: {
+      turbo: turboPlugin,
+    },
+    rules: {
+      "turbo/no-undeclared-env-vars": "warn",
+    },
+  },
+  {
+    plugins: {
+      onlyWarn,
+    },
+  },
+  {
+    ...pluginReact.configs.flat.recommended,
+    languageOptions: {
+      ...pluginReact.configs.flat.recommended.languageOptions,
+      globals: {
+        ...globals.serviceworker,
+      },
+    },
+  },
+  {
+    plugins: {
+      "@next/next": pluginNext,
+    },
+    rules: {
+      ...pluginNext.configs.recommended.rules,
+      ...pluginNext.configs["core-web-vitals"].rules,
+    },
+  },
+  {
+    plugins: {
+      "react-hooks": pluginReactHooks,
+    },
+    settings: { react: { version: "detect" } },
+    rules: {
+      ...pluginReactHooks.configs.recommended.rules,
+      // React scope no longer necessary with new JSX transform.
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+];
+
+export default config;
